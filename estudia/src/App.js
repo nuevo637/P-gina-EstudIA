f765e42c1a3cd7643914a7c4bc957ff610bde4c2
Jeans Anthony Ajra Huacso <ajrahuacso@gmail.com>
2024-08-13 16:16:30 -0500

    Se añaden los componentes al app

--- estudia/src/App.js
+++ estudia/src/App.js
@@ -1,10 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import { Acerca } from './components/acerca/Acerca';
+import { Inicio } from './components/inicio/Inicio';
+import { Beneficiarios } from './components/beneficiarios/Beneficiarios';
+import { Footer } from './components/footer/Footer';
 
 function App() {
   return (
     <div className="App">
-      
+      <Nav></Nav>
+      <Inicio></Inicio>
+      <Acerca></Acerca>  
+      <Beneficiarios></Beneficiarios>
+      <Footer></Footer>
     </div>
   );
 }
