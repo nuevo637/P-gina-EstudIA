acbe7ee0478c32e45128e006407aabda90fe010d
Choflis <lluquecon@unsa.edu.pe>
2024-08-13 19:31:05 -0500

    primera seccion de beneficiarios

--- estudia/src/components/beneficiarios/Beneficiarios.js
+++ estudia/src/components/beneficiarios/Beneficiarios.js
@@ -1,7 +1,23 @@
-import React from 'react'
+import React from 'react';
+import './Beneficiarios.css';
+import globeImage from '../../assets/images/countries.png';
 
 export const Beneficiarios = () => {
   return (
-    <div>Beneficiarios</div>
-  )
-}
+    <section className="beneficiarios-container">
+      <h2 className="beneficiarios-title">BENEFICIARIOS</h2>
+      <hr className="beneficiarios-divider" />
+      <p className="beneficiarios-question">
+        ¿A qué grupo impacta nuestra idea innovadora y de qué manera lo hace?
+      </p>
+      <div className="beneficiarios-content">
+        <p className="beneficiarios-description">
+          EstudIA brinda a los estudiantes una experiencia interactiva y dinámica, facilitando el aprendizaje a través de un chat que responde dudas en diversas áreas de estudio. Además, simula interacciones con personajes destacados, lo que ayuda a los estudiantes de educación básica a acceder de manera más sencilla a las nuevas tecnologías de inteligencia artificial.
+        </p>
+        <div className="beneficiarios-image">
+          <img src={globeImage} alt="Banderas" className="rotating-globe" />
+        </div>
+      </div>
+    </section>
+  );
+};
