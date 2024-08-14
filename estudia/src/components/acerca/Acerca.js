df460f35f68ac832a9e40ee19c4eedbd44bd4dde
Jeans Anthony Ajra Huacso <ajrahuacso@gmail.com>
2024-08-13 21:51:08 -0500

    Acerca version 1

--- estudia/src/components/acerca/Acerca.js
+++ estudia/src/components/acerca/Acerca.js
@@ -6,12 +6,11 @@ import img4 from '../../assets/images/img4.png';
 import img5 from '../../assets/images/img5.png';
 import img6 from '../../assets/images/img6.png';
 
-
 export const Acerca = () => {
   return (
     <section className="container-fluid" style={{ backgroundColor: '#FFFCBB' }}>
       <div className="container-sm ps-5 pe-5" style={{ maxWidth: '900px', margin: '0 auto' }}>
-        <h3 className="text-center pt-3 pb-2">ACERCA</h3>
+        <h3 className="text-center pt-5 pb-2">ACERCA</h3>
         <hr style={{
           border: '0',
           borderRadius: '10px',
@@ -21,13 +20,13 @@ export const Acerca = () => {
           margin: '10px auto',
           opacity: '1' 
         }} />
-        <p className="text-center pt-2 pb-3">
+        <p className="text-center pt-2 pb-3 fs-5">
           EstudIA es una plataforma web en la que los estudiantes pueden interactuar 
           directamente con diferentes chatbots de inteligencia artificial para poder 
           resolver dudas respecto a diferentes temas del colegio.
         </p>
 
-        <p className="text-center pt-2 pb-4" style={{ maxWidth: '500px', margin: '0 auto' }}>
+        <p className="text-center pt-2 pb-4 fs-5" style={{ maxWidth: '500px', margin: '0 auto' }}>
         Los estudiantes podrán interactuar con IAs especializadas en los siguientes temas:
         </p>
 
@@ -41,7 +40,7 @@ export const Acerca = () => {
                 height: '200px'
               }}
             > 
-              <p className='text-center mb-0'>Matemáticas</p>
+              <p className='text-center mb-0 fs-4 text-white'>Matemáticas</p>
             </div>
             <div className='col d-flex align-items-center justify-content-center' 
               style={{
@@ -51,7 +50,7 @@ export const Acerca = () => {
                 height: '200px'
               }}
             > 
-              <p className='text-center mb-0'>Ciencias y Tecnología</p>
+              <p className='text-center mb-0 fs-4 text-white mx-5 px-5'>Ciencias y Tecnología</p>
             </div>
           </div>
 
@@ -64,7 +63,7 @@ export const Acerca = () => {
                 height: '200px'
               }}
             > 
-              <p className='text-center mb-0'>Matemáticas</p>
+              <p className='text-center mb-0 fs-4 text-white'>Historia</p>
             </div>
             <div className='col d-flex align-items-center justify-content-center' 
               style={{
@@ -74,7 +73,7 @@ export const Acerca = () => {
                 height: '200px'
               }}
             > 
-              <p className='text-center mb-0'>Matemáticas</p>
+              <p className='text-center mb-0 fs-4 text-white'>Ingles</p>
             </div>
           </div>
 
@@ -87,7 +86,7 @@ export const Acerca = () => {
                 height: '200px'
               }}
             > 
-              <p className='text-center mb-0'>Matemáticas</p>
+              <p className='text-center mb-0 fs-4 text-white'>Comunicación</p>
             </div>
             <div className='col d-flex align-items-center justify-content-center' 
               style={{
@@ -97,12 +96,12 @@ export const Acerca = () => {
                 height: '200px'
               }}
             > 
-              <p className='text-center mb-0'>Matemáticas</p>
+              <p className='text-center mb-0 fs-4 text-white'>Arte</p>
             </div>
           </div>
         </div>
 
-        <p className="text-center pt-5 pb-5" style={{ maxWidth: '500px', margin: '0 auto' }}>
+        <p className="text-center pt-5 pb-5 fs-5" style={{ maxWidth: '700px', margin: '0 auto' }}>
           Los alumnos podrán preguntar a un personaje histórico acerca de sus hazañas, a un profesor de matemática sobre ejemplos de un tipo de problemas, a un animal en peligro de extinción acerca de sus características y entre muchas cosas más.
         </p>
 
