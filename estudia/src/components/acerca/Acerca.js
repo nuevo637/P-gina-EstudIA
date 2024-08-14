b7fa10d394805add1136b0752e726c5a0b63c485
Jeans Anthony Ajra Huacso <ajrahuacso@gmail.com>
2024-08-13 21:27:01 -0500

    Configuracion de plantilla

--- estudia/src/components/acerca/Acerca.js
+++ estudia/src/components/acerca/Acerca.js
@@ -1,4 +1,11 @@
 import React from 'react'
+import img1 from '../../assets/images/img1.png';
+import img2 from '../../assets/images/img2.png';
+import img3 from '../../assets/images/img3.png';
+import img4 from '../../assets/images/img4.png';
+import img5 from '../../assets/images/img5.png';
+import img6 from '../../assets/images/img6.png';
+
 
 export const Acerca = () => {
   return (
@@ -20,10 +27,81 @@ export const Acerca = () => {
           resolver dudas respecto a diferentes temas del colegio.
         </p>
 
-        <p className="text-center pt-2 pb-3" style={{ maxWidth: '500px', margin: '0 auto' }}>
+        <p className="text-center pt-2 pb-4" style={{ maxWidth: '500px', margin: '0 auto' }}>
         Los estudiantes podrán interactuar con IAs especializadas en los siguientes temas:
         </p>
 
+        <div className='container' style={{maxWidth: '700px'}}>
+          <div className='row' style={{ height: '200px' }}>
+            <div className='col d-flex align-items-center justify-content-center' 
+              style={{
+                backgroundImage: `url(${img1})`,
+                backgroundSize: 'cover',
+                backgroundPosition: 'center',
+                height: '200px'
+              }}
+            > 
+              <p className='text-center mb-0'>Matemáticas</p>
+            </div>
+            <div className='col d-flex align-items-center justify-content-center' 
+              style={{
+                backgroundImage: `url(${img2})`,
+                backgroundSize: 'cover',
+                backgroundPosition: 'center',
+                height: '200px'
+              }}
+            > 
+              <p className='text-center mb-0'>Ciencias y Tecnología</p>
+            </div>
+          </div>
+
+          <div className='row' style={{ height: '200px' }}>
+            <div className='col d-flex align-items-center justify-content-center' 
+              style={{
+                backgroundImage: `url(${img3})`,
+                backgroundSize: 'cover',
+                backgroundPosition: 'center',
+                height: '200px'
+              }}
+            > 
+              <p className='text-center mb-0'>Matemáticas</p>
+            </div>
+            <div className='col d-flex align-items-center justify-content-center' 
+              style={{
+                backgroundImage: `url(${img4})`,
+                backgroundSize: 'cover',
+                backgroundPosition: 'center',
+                height: '200px'
+              }}
+            > 
+              <p className='text-center mb-0'>Matemáticas</p>
+            </div>
+          </div>
+
+          <div className='row' style={{ height: '200px' }}>
+          <div className='col d-flex align-items-center justify-content-center' 
+              style={{
+                backgroundImage: `url(${img5})`,
+                backgroundSize: 'cover',
+                backgroundPosition: 'center',
+                height: '200px'
+              }}
+            > 
+              <p className='text-center mb-0'>Matemáticas</p>
+            </div>
+            <div className='col d-flex align-items-center justify-content-center' 
+              style={{
+                backgroundImage: `url(${img6})`,
+                backgroundSize: 'cover',
+                backgroundPosition: 'center',
+                height: '200px'
+              }}
+            > 
+              <p className='text-center mb-0'>Matemáticas</p>
+            </div>
+          </div>
+          
+        </div>
       </div>
     </section>
   );
