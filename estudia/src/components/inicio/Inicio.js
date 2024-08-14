ab98fcfd36f81545c588942c16b2aed61b10fcb5
FernandoGarambelM <fgarambel@unsa.edu.pe>
2024-08-13 19:42:28 -0500

    Componente inicio

--- estudia/src/components/inicio/Inicio.js
+++ estudia/src/components/inicio/Inicio.js
@@ -2,6 +2,46 @@ import React from 'react'
 
 export const Inicio = () => {
   return (
-    <div>Inicio</div>
+    <div className="container my-5">
+      <div className="row">
+        {/* Columna Izquierda Superior */}
+        <div className="col-md-6 d-flex justify-content-center align-items-center">
+          <img
+            src="/images/ilustracion1.png"
+            alt="Niños con computadora"
+            className="img-fluid"
+          />
+        </div>
+        
+        {/* Columna Derecha Superior */}
+        <div 
+          className="col-md-6 d-flex flex-column justify-content-center align-items-start p-4 rounded" 
+          style={{ backgroundColor: '#FAF4D2' }}
+        >
+          <h1 className="display-4">¡Bienvenidos a <strong>estudIA</strong>!</h1>
+          <p className="lead">
+            La plataforma virtual donde los niños pueden explorar, aprender y divertirse con ayuda de la inteligencia artificial.
+          </p>
+        </div>
+      </div>
+
+      <div className="row mt-5">
+        {/* Columna Izquierda Inferior */}
+        <div className="col-md-6 d-flex justify-content-center align-items-center">
+          <img
+            src="/images/Logo_estudIA.png"
+            alt="Icono didáctico"
+            className="img-fluid"
+          />
+        </div>
+        
+        {/* Columna Derecha Inferior */}
+        <div className="col-md-6 d-flex flex-column justify-content-center align-items-start">
+          <p className="lead">
+            En estudIA, ofrecemos un espacio seguro y entretenido donde los niños pueden interactuar con contenido educativo en áreas como Matemáticas, Historia, Ciencias, Inglés, Arte y más. Aquí, aprender es una aventura llena de descubrimientos y diversión.
+          </p>
+        </div>
+      </div>
+    </div>
   )
 }
